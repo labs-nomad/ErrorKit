cc8422e9549f8f53189c2d5037677d7a276c54cb
Nomad Company <Nomad@Jons-MacBook-Pro-2.local>
2020-09-23 17:41:54 -0700

    Modified Name

--- Package.swift
+++ Package.swift
@@ -4,15 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "ErrorKit",
+    name: "NMErrorKit",
     platforms: [
         .iOS(SupportedPlatform.IOSVersion.v13)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "ErrorKit",
-            targets: ["ErrorKit"]),
+            name: "NMErrorKit",
+            targets: ["NMErrorKit"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -22,10 +22,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "ErrorKit",
+            name: "NMErrorKit",
             dependencies: []),
         .testTarget(
-            name: "ErrorKitTests",
-            dependencies: ["ErrorKit"]),
+            name: "NMErrorKitTests",
+            dependencies: ["NMErrorKit"]),
     ]
 )
